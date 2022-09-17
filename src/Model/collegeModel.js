250c4f2bbb5d66e186b27bc95ec9847c08d79147
yachika03 <yachikamehta03@gmail.com>
2022-09-17 20:54:11 +0530

    add

---
+++ src/Model/collegeModel.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+const collegeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    fullName: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    logoLink: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    isDeleted: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true })
+module.exports = mongoose.model('college', collegeSchema)
